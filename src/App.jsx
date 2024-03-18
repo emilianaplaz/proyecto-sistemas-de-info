854f918738dedbd8b2aa704ff3dbc8bb1e2c2b4c
ariannespg <Arianne.perretgentil@correo.unimet.edu.ve>
2024-03-18 11:02:33 -0400

    Prueba 1 😖
    tratando de que no salga error

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Hola, Prueba 1
+    </div>
+  )
+}
+
+export default App
+
+
+
+
 
 
