9168eee0776541d70977ea2b5cf6a5b5bb046aa5
isaapereiraa <isabella.pereira@correo.unimet.edu.ve>
2024-03-28 16:55:27 -0400

    Paypal medio listo

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -4,6 +4,7 @@ import Navbar from '../Components/Navbar';
 import Login from '../Pages/Login';
 import Home from '../Pages/Home';
 import Account from '../Pages/Account'; 
+import Paypal from '../Pages/Paypal';
 
 export const AppRouter = () => {
 	return (
@@ -13,6 +14,7 @@ export const AppRouter = () => {
 				<Route index element={<Home />} />
 				<Route path='login' element={<Login />} />
 				<Route path='account' element={<PrivateRoute><Account /></PrivateRoute>} />
+				<Route path='paypal' element={<Paypal />} />
 			</Routes>
 		</>
 	);
